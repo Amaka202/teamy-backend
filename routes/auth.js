e29197b30f1d1e228b3d102516435139c15111e7
Amaka <amakaumeh01@gmail.com>
2020-12-26 14:28:06 +0100

    rename a file

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-const { signUpUser } = require('../controllers/auth');
-const { loggIn } = require('../controllers/auth');
+const { signUpUser } = require('../controllers/authController');
+const { loggIn } = require('../controllers/authController');
 
 router.post('/signup', signUpUser);
 
