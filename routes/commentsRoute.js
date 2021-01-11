b03b86d79dd14a21ac524a69882e147e7c744709
Amaka <amakaumeh01@gmail.com>
2021-01-11 10:55:47 +0100

    update comments router

--- routes/commentsRoute.js
+++ routes/commentsRoute.js
@@ -4,8 +4,12 @@ const router = express.Router({ mergeParams: true });
 
 const postComments = require('../controllers/commentsController');
 
+const getComments = require('../controllers/commentsController');
+
 const checkToken = require('../middleware/checkToken');
 
 router.post('/posts/:postId/comment', checkToken, postComments);
+
+router.get('/posts/:postId/comment', checkToken, getComments);
 
 module.exports = router;
